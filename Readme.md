8cb7ec57130c2ba7c5c125db74753c3f82005822
Alex Merced <willcodeforfood@alexmerced.com>
2020-09-08 11:43:33 -0400

    first comit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## USING THIS TEMPLATE:
+
+- Environmental variables defined in env.yaml
+
+- For production you should add a store property to sessions that'll store the session in redis or mongo. See the express-session documentation to learn more.
+
+MAKE SURE TO JOIN THE SLACK AND DISCORD COMMUNITIES AT DEVNURSERY.COM
---
+++ server.js
@@ -0,0 +1,48 @@
+//ENVIRONMENT VARIABLES FROM env.yaml
+const yenv = require('yenv')
+const env = yenv('env.yaml', { env: process.env.NODE_ENV })
+const {PORT=process.env.PORT, SECRET} = env
+
+//Bringing in Express
+const express = require('express')
+const app = express()
+
+//OTHER IMPORTS
+const session = require('express-session')
+const methodOverride = require('method-override')
+const morgan = require('morgan')
+
+//set view engine to express-react-views
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+
+////////////
+//MIDDLEWARE
+////////////
+app.use(session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: process.env.NODE_ENV === "production" }
+  }))
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true})) //comment if not using forms
+// app.use(express.json()) uncomment if using json
+app.use(morgan('tiny')) //logging
+
+
+///////////////
+//Routes and Routers
+//////////////
+app.get('/', (req, res) => {
+    res.render('index.jsx', {hello: "Hello World"})
+})
+
+
+
+
+//LISTENER
+app.listen(PORT, () => {
+    console.log(`Your are listening on port ${PORT}`)
+})
